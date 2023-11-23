8baa6175b5d6891c04e08dd48f2a88fecf1fa7d6
Shahadat-Git <noyon560@gmail.com>
2023-11-23 13:35:53 +0600

    connected to mongodb with mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
+import mongoose from "mongoose";
 import app from "./app"
 import config from "./app/config";
 
 async function main() {
     try {
+
+        await mongoose.connect(config.database_url as string);
+        console.log('connect to mongoDB');
         app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
+            console.log(`app listening on port ${config.port}`)
         })
     } catch (error) {
         console.log(error);
